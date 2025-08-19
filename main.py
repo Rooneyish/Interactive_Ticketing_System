aad3ad7f53a6908f94a7db3f530f6f8f01239296
rooneyish <ilustraterp@gmail.com>
2025-08-19 14:34:31 +0545

    Dashboard of Ticket Analytics

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from ticket_manager import display
+from ticket_manager import display, collection, dashboard
 
 def menu():
     while True:
         print("\n===== Help Desk Ticket System =====")
         print("1. Display Tickets")
+        print("2. Display Dashboard")
         print("0. Exit")
 
         choice = input("Enter your choice: ")
-
+        data = collection()
+        
         if choice == "1":
-            display()
+            display(data)
+        if choice == "2":
+            dashboard(data)
         if choice == "0":
             return
 menu()
--- ticket_manager.py
+++ ticket_manager.py
@@ -1,8 +1,4 @@
-def display():
-    file = open("tickets.txt", "r")
-    table = [line.strip().split(",") for line in file]
-    file.close()
-
+def display(table):
     columns = list(zip(*table))
     col_widths =[]
     for col in columns:
@@ -17,3 +13,35 @@ def display():
                 line += " | "
         print(line)
 
+def collection():
+    file = open("tickets.txt", "r")
+    dataList = []
+    for eachLine in file:
+        eachLine = eachLine.strip().split(",")
+        dataList.append(eachLine)
+    file.close()
+    return dataList
+
+def dashboard(data):
+    status_counts = {"open": 0, "closed": 0}
+    priority_counts = {"low": 0, "medium":  0, "high": 0}
+
+    for row in data[1:]:
+        status = row[2].lower()
+        priority = row[3].lower()
+
+        if status in status_counts:
+            status_counts[status] += 1
+        if priority in priority_counts:
+            priority_counts[priority] += 1
+        
+    print("\n Ticket Analytics Dashboard")
+    print("="*40)
+    print("Ticket Status:")
+    for status, count in status_counts.items():
+        print(f"  {status.capitalize():<7}: {count}")
+
+    print("\nPriority Levels:")
+    for priority, count in priority_counts.items():
+        print(f"  {priority.capitalize():<7}: {count}")
+    print("="*40)
