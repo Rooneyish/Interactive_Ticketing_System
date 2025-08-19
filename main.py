1157684b67d51c17020522c2f5112f27f7e72bfa
rooneyish <ilustraterp@gmail.com>
2025-08-19 17:45:18 +0545

    Add Ticket Functionality

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from ticket_manager import display, collection, dashboard
+from ticket_manager import display, collection, dashboard, add_tickets
 
 def menu():
     while True:
         print("\n===== Help Desk Ticket System =====")
         print("1. Display Tickets")
         print("2. Display Dashboard")
+        print("3. Add new Ticket")
         print("0. Exit")
 
         choice = input("Enter your choice: ")
@@ -14,6 +15,8 @@ def menu():
             display(data)
         if choice == "2":
             dashboard(data)
+        if choice == "3":
+            add_tickets()
         if choice == "0":
             return
 menu()
--- ticket_manager.py
+++ ticket_manager.py
@@ -9,7 +9,7 @@ def display(table):
         line = "| "
         for i, item in enumerate(row):
             line += "{:<{width}}".format(item, width = col_widths[i])
-            if i < len(row):
+            if i < len(row)-1:
                 line += " | "
         print(line)
 
@@ -45,3 +45,39 @@ def dashboard(data):
     for priority, count in priority_counts.items():
         print(f"  {priority.capitalize():<7}: {count}")
     print("="*40)
+
+def add_tickets():
+    table = collection()
+
+    exiting_ids = [int(row[0]) for row in table[1:]]
+
+    new_id = max(exiting_ids) + 1 if exiting_ids else 1
+
+    while True:
+        title = input("Enter Title: ")
+        if title == "":
+            print("Title cannot be empty.")
+        else: 
+            break
+    
+    while True:
+        status = input("Enter Status ('o' for  open/'c' for closed): ").lower()
+        if status not in ["o", "c"]:
+            print("Invalid status. Enter 'o' or 'c'")
+        else:
+            status = "open" if status == "o" else "closed"
+            break
+    
+    while True:
+        priority = input("Enter Priority ('l' for low/ 'm' for medium/'h' for high): ").lower()
+        if priority not in ["l", "m", "h"]:
+            print("Invalid priority. Enter 'l', 'm' or 'h'")
+        else: 
+            priority = "low" if priority == "l" else "medium" if priority == 'm' else 'high'
+            break
+    
+    file = open("tickets.txt", 'a')
+    file.write(f"\n{new_id},{title},{status},{priority}")
+    file.close()
+
+    print("Ticket added successfully!")
